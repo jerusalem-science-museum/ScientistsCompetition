647e0600478802861bee5ea504a56f76a6eb414b
Idan Kimel <idan.miller@gmail.com>
2018-08-08 23:43:11 +0300

    Fixed file uploading and completely redundant usage of firebase realtime database....

--- RegistrationWebApp/src/app/services/upload-file.service.ts
+++ RegistrationWebApp/src/app/services/upload-file.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFirestore } from 'angularfire2/firestore';
 import * as firebase from 'firebase';
 
 import { FileUpload } from '../fileupload';
@@ -9,61 +10,35 @@ export class UploadFileService {
 
   public basePath;
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private afs: AngularFirestore) { }
 
   pushFileToStorage(fileUpload: FileUpload, progress: { percentage: number }) {
 
     return new Promise((resolve, reject) => {
-    const storageRef = firebase.storage().ref();
-    const uploadTask = storageRef.child(`${this.basePath}/${fileUpload.file.name}`).put(fileUpload.file);
+      const storageRef = firebase.storage().ref();
+      const uploadTask = storageRef.child(`${this.basePath}/${fileUpload.file.name}`).put(fileUpload.file);
 
-    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-      (snapshot) => {
-        // in progress , raise the progress bar
-        const snap = snapshot as firebase.storage.UploadTaskSnapshot;
-        progress.percentage = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);
-      },
-      (error) => {
-        // fail
-        console.log(error);
-      },
-      () => {
-        // success, finished upload , now the save the file
-        fileUpload.url = uploadTask.snapshot.downloadURL;
-        fileUpload.name = fileUpload.file.name;
-        this.saveFileData(fileUpload);
-        resolve();
-      }
-    );
-  });
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot) => {
+          // in progress , raise the progress bar
+          const snap = snapshot as firebase.storage.UploadTaskSnapshot;
+          progress.percentage = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);
+        },
+        (error) => {
+          // fail
+          console.log(error);
+        },
+        () => {
+          // success, finished upload , now the save the file
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            fileUpload.url = downloadURL;
+            fileUpload.name = fileUpload.file.name;
+            console.log(fileUpload);
+            resolve();
+          };
+        }
+      );
+    });
   }
-
-  private saveFileData(fileUpload: FileUpload) {
-    this.db.list(`${this.basePath}/`).push(fileUpload);
-  }
-//========================================//
-  getFileUploads(numberItems): AngularFireList<FileUpload> {
-    return this.db.list(this.basePath, ref =>
-      ref.limitToLast(numberItems));
-  }
-//================DELETE==================//
-  public deleteFileUpload(fileUpload: FileUpload) {
-    this.deleteFileDatabase(fileUpload.key)
-      .then(() => {
-        this.deleteFileStorage(fileUpload.name);
-      })
-      .catch(error => console.log(error));
-  }
-
-  private deleteFileDatabase(key: string) {
-    return this.db.list(`${this.basePath}/`).remove(key);
-  }
-
-  private deleteFileStorage(name: string) {
-    const storageRef = firebase.storage().ref();
-    storageRef.child(`${this.basePath}/${name}`).delete();
-  }
-//==========================================//
-
 }
 
