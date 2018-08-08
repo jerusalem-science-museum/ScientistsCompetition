90d63e7460620ff4894e6bed26a9708945616a0e
Idan Kimel <idan.miller@gmail.com>
2018-08-08 15:20:14 +0300

    Fixed home page not updating when submitting a project

--- RegistrationWebApp/src/app/user-home-page/user-home-page.component.ts
+++ RegistrationWebApp/src/app/user-home-page/user-home-page.component.ts
@@ -18,7 +18,7 @@ export class UserHomePageComponent implements OnInit {
 
   userProject: Project;
   emptyFields = [];
-  workExists = 'עליך לצרף עבודה למערכת'
+  workExists = 'עליך לצרף עבודה למערכת';
   user_projects = [];
   missing_details = [];
   missing_file = [];
@@ -56,68 +56,70 @@ export class UserHomePageComponent implements OnInit {
       }
     })
     this.db.getProjectMetaData().subscribe((res) => {
-      this.db.projectsList = res;
-      for (var i = 0; i < this.db.projectsList.length; i++) {
-        if (this.db.projectsList[i].id == this.db.loggedInUser.project)
-          this.userProject = this.db.projectsList[i];
-      }
-      if (this.userProject != undefined && this.userProject != 'not found') {
-        this.workExists = 'קיימת עבודה במערכת'
-        if (this.userProject.project_file == undefined)
-          this.emptyFields.push('עדיין לא הועלה קובץ פרויקט')
-        if (this.userProject.location == undefined || this.userProject.location == '')
-          this.emptyFields.push('מוסד אקדמי בו התבצעה העבודה')
-
-        //=============================================================================================//
-        if (this.userProject.advantages == undefined || this.userProject.advantages == '')
-          this.emptyFields.push('יתרונות/תרומה')
-        if (this.userProject.background == undefined || this.userProject.background == '')
-          this.emptyFields.push('רקע')
-        if (this.userProject.description == undefined || this.userProject.description == '')
-          this.emptyFields.push('תיאור קצר')
-        if (this.userProject.inovetion == undefined || this.userProject.inovetion == '')
-          this.emptyFields.push('חדשנות')
-        if (this.userProject.retrospective == undefined || this.userProject.retrospective == '')
-          this.emptyFields.push('נקודת מבט אישית')
-        if (this.userProject.scope == undefined || this.userProject.scope == '')
-          this.emptyFields.push('היקף')
-        if (this.userProject.target == undefined || this.userProject.target == '')
-          this.emptyFields.push('שאלת מחקר/מטרת הפרויקט')
-        if ((this.userProject.products == undefined || this.userProject.products == '') && this.userProject.type == 'עבודה טכנולוגית')
-          this.emptyFields.push('מוצרים דומים הקיימים בשוק')
-        if (this.userProject.finishTime == undefined || this.userProject.target == '')
-          this.emptyFields.push('הערכת זמן')
-        if ((this.userProject.researchStatus == undefined || this.userProject.researchStatus == '') && this.userProject.type == 'מחקרית')
-          this.emptyFields.push('סטטום מחקר')
-        if ((this.userProject.modelStatus == undefined || this.userProject.modelStatus == '') && this.userProject.type == 'עבודה טכנולוגית')
-          this.emptyFields.push('סטטום דגם')
-        //=============================================================================================//
-        if (this.userProject.isMentors == false && (this.userProject.mentor1.name == undefined || this.userProject.mentor1.name == ''))
-          this.emptyFields.push('שם מנחה')
-        if (this.userProject.isMentors == false && (this.userProject.mentor1.phone == undefined || this.userProject.mentor1.phone == ''))
-          this.emptyFields.push('טלפון מנחה')
-        if (this.userProject.isMentors == false && (this.userProject.mentor1.email == undefined || this.userProject.mentor1.email == ''))
-          this.emptyFields.push('מייל מנחה')
-      }
-      var j = 0, k = 0, f = 0, r = 0;
-      for (var i = 0; i < this.db.projectsList.length; i++) {
-        if (this.db.projectsList[i].school_contact_mail == this.db.loggedInUser.email) {
-          this.user_projects[j++] = this.db.projectsList[i].project_name;
-          if (this.db.projectsList[i].submission == false)
-            this.missing_details[k++] = 'חסרים פרטים'
-          else if (this.db.projectsList[i].submission == true)
-            this.missing_details[k++] = 'כל הפרטים מלאים'
-          if (this.db.projectsList[i].project_file == undefined)
-            this.missing_file[f++] = 'חסר קובץ'
-          else if (this.db.projectsList[i].project_file != undefined)
-            this.missing_file[f++] = 'הועלה קובץ'
-          if (this.db.projectsList[i].recommendation_file == undefined)
-            this.missing_recommendation[r++] = 'חסר המלצה'
-          else if (this.db.projectsList[i].recommendation_file != undefined)
-            this.missing_recommendation[r++] = 'הועלה קובץ המלצה'
-          //recommendation_file
+      this.db.getLoggedInUser().then(()=>{
+        this.db.projectsList = res;
+        for (var i = 0; i < this.db.projectsList.length; i++) {
+          if (this.db.projectsList[i].id == this.db.loggedInUser.project)
+            this.userProject = this.db.projectsList[i];
         }
-      }
+        if (this.userProject != undefined && this.userProject != 'not found') {
+          this.workExists = 'קיימת עבודה במערכת'
+          if (this.userProject.project_file == undefined)
+            this.emptyFields.push('עדיין לא הועלה קובץ פרויקט')
+          if (this.userProject.location == undefined || this.userProject.location == '')
+            this.emptyFields.push('מוסד אקדמי בו התבצעה העבודה')
+
+          //=============================================================================================//
+          if (this.userProject.advantages == undefined || this.userProject.advantages == '')
+            this.emptyFields.push('יתרונות/תרומה')
+          if (this.userProject.background == undefined || this.userProject.background == '')
+            this.emptyFields.push('רקע')
+          if (this.userProject.description == undefined || this.userProject.description == '')
+            this.emptyFields.push('תיאור קצר')
+          if (this.userProject.inovetion == undefined || this.userProject.inovetion == '')
+            this.emptyFields.push('חדשנות')
+          if (this.userProject.retrospective == undefined || this.userProject.retrospective == '')
+            this.emptyFields.push('נקודת מבט אישית')
+          if (this.userProject.scope == undefined || this.userProject.scope == '')
+            this.emptyFields.push('היקף')
+          if (this.userProject.target == undefined || this.userProject.target == '')
+            this.emptyFields.push('שאלת מחקר/מטרת הפרויקט')
+          if ((this.userProject.products == undefined || this.userProject.products == '') && this.userProject.type == 'עבודה טכנולוגית')
+            this.emptyFields.push('מוצרים דומים הקיימים בשוק')
+          if (this.userProject.finishTime == undefined || this.userProject.target == '')
+            this.emptyFields.push('הערכת זמן')
+          if ((this.userProject.researchStatus == undefined || this.userProject.researchStatus == '') && this.userProject.type == 'מחקרית')
+            this.emptyFields.push('סטטום מחקר')
+          if ((this.userProject.modelStatus == undefined || this.userProject.modelStatus == '') && this.userProject.type == 'עבודה טכנולוגית')
+            this.emptyFields.push('סטטום דגם')
+          //=============================================================================================//
+          if (this.userProject.isMentors == false && (this.userProject.mentor1.name == undefined || this.userProject.mentor1.name == ''))
+            this.emptyFields.push('שם מנחה')
+          if (this.userProject.isMentors == false && (this.userProject.mentor1.phone == undefined || this.userProject.mentor1.phone == ''))
+            this.emptyFields.push('טלפון מנחה')
+          if (this.userProject.isMentors == false && (this.userProject.mentor1.email == undefined || this.userProject.mentor1.email == ''))
+            this.emptyFields.push('מייל מנחה')
+        }
+        var j = 0, k = 0, f = 0, r = 0;
+        for (var i = 0; i < this.db.projectsList.length; i++) {
+          if (this.db.projectsList[i].school_contact_mail == this.db.loggedInUser.email) {
+            this.user_projects[j++] = this.db.projectsList[i].project_name;
+            if (this.db.projectsList[i].submission == false)
+              this.missing_details[k++] = 'חסרים פרטים'
+            else if (this.db.projectsList[i].submission == true)
+              this.missing_details[k++] = 'כל הפרטים מלאים'
+            if (this.db.projectsList[i].project_file == undefined)
+              this.missing_file[f++] = 'חסר קובץ'
+            else if (this.db.projectsList[i].project_file != undefined)
+              this.missing_file[f++] = 'הועלה קובץ'
+            if (this.db.projectsList[i].recommendation_file == undefined)
+              this.missing_recommendation[r++] = 'חסר המלצה'
+            else if (this.db.projectsList[i].recommendation_file != undefined)
+              this.missing_recommendation[r++] = 'הועלה קובץ המלצה'
+            //recommendation_file
+          }
+        }
+      })
     })
   }
 
